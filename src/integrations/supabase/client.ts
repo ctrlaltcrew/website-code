e45c583a526c0f3fdebff0de3030ac386a4e2f22
ctrlaltcrew <ctrlaltcrew@gmail.com>
2026-01-31 22:55:01 +0500

    Update Supabase credentials to new project

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 import type { Database } from './types';
 
-const SUPABASE_URL = "https://vdvtfqzybecwdaozfynr.supabase.co";
-const SUPABASE_PUBLISHABLE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZkdnRmcXp5YmVjd2Rhb3pmeW5yIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjc5NzEwODcsImV4cCI6MjA0MzU0NzA4N30.kk1FpLnWy0njKhb5r61Ycg3hqMjEXDRSGPFEIGRFRp4";
+const SUPABASE_URL = "https://csuurssjasorihjbhigc.supabase.co";
+const SUPABASE_PUBLISHABLE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNzdXVyc3NqYXNvcmloamJoaWdjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk4Nzc0NjQsImV4cCI6MjA4NTQ1MzQ2NH0.CD64S7Qc4v2XR8X-D8v2TCyeYqqlwlIU2k062oDl9sc";
 
 export const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY);
